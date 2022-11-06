a4d8d9f8c61e05ac8692dd09383a7dc17bafe6a1
Josefine Magnussen <josefine.magnussen@hotmail.com>
2022-11-06 15:02:31 +0100

    add some new style

---
+++ src/pages/Register/RegisterUser.js
@@ -0,0 +1,11 @@
+import Heading from "../../components/Common/Heading";
+import RegisterUserForm from "../../components/register/RegisterUserForm";
+
+export default function RegisterUser() {
+  return (
+    <>
+      <Heading title="Register" />
+      <RegisterUserForm />
+    </>
+  );
+}
