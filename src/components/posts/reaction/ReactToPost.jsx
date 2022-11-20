fef91d67150cbccad16a2cad51b45657c8e41818
Josefine Magnussen <josefine.magnussen@hotmail.com>
2022-11-20 21:20:23 +0100

    deleted file

--- src/components/posts/reaction/ReactToPost.jsx
+++
@@ -1,9 +0,0 @@
-import GetPostReaction from "./GetReaction";
-
-export default function ReactToPost(props){
-  return(<>
-    <GetPostReaction id={props.id}/>
-    <p>A registered user may react to any Post with an emoji</p>
-  </>
-  )
-}
