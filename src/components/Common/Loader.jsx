15458dc601a07cbbb5d9af018f60f8f7e95220e3
Josefine Magnussen <josefine.magnussen@hotmail.com>
2022-11-22 20:35:34 +0100

    removed text from loader

--- src/components/Common/Loader.jsx
+++ src/components/Common/Loader.jsx
@@ -3,7 +3,7 @@ import Spinner from 'react-bootstrap/Spinner';
 export default function Loader() {
   return (
       <Spinner animation="border" role="status">
-        <span className="visually-hidden">Loading...</span>
+        <span className="visually-hidden"></span>
       </Spinner>
   );
 }
