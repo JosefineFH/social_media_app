487b046630d9df590ecc3ec0cc272426b16607c9
Josefine Magnussen <josefine.magnussen@hotmail.com>
2022-11-28 18:28:23 +0100

    fetch tags

--- src/components/profile/PostsList.jsx
+++ src/components/profile/PostsList.jsx
@@ -7,7 +7,7 @@ import { BASE_URL } from "../../constants/api";
 import { Link } from "react-router-dom";
 import DeletePost from "../posts/DeletePost";
 
-export default function GetPostsList(){
+export default function GetPostsList() {
   // const navigate = useNavigate();
   const [auth, setAuth] = useContext(AuthContext);
   const userName = auth.name
@@ -20,41 +20,53 @@ export default function GetPostsList(){
 
   useEffect(function () {
     async function getPosts() {
-      if (auth === null) {
-        // navigate('/')
-      } else {
-        const token = auth.accessToken;
-        const options = {
-          headers: { Authorization: `Bearer ${token}` },
-        };
-        try {
-          const response = await axios.get(url, options);
-          const postsData = response.data;
-            setPosts(postsData);
-            console.log(postsData)
-          
-        } catch (error) {
-          setIsError("There was an error fetching your posts");
-        } finally {
-          setIsLoading(false);
-        }
+      const token = auth.accessToken;
+      const options = {
+        headers: { Authorization: `Bearer ${token}` },
+      };
+      try {
+        const response = await axios.get(url, options);
+        const postsData = response.data;
+        setPosts(postsData);
+      } catch (error) {
+        setIsError("There was an error fetching your posts");
+      } finally {
+        setIsLoading(false);
       }
+
     }
 
     getPosts();
   }, []);
-  if(posts.length === 0){
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+  if (isError) {
+    return <div>{isError}</div>;
+  }
+
+
+  if (posts.length === 0) {
     return <p>You don't have any posts. Add your first post <a href="/createposts">her!</a></p>;
-  }else{
-    return(
+  } else {
+    return (
       <>
-        {posts.map((post) => {  
-          return(
-            <div>
-              <h2>{post.title}</h2>
-              <DeletePost onClick={DeletePost} key={post.id} id={post.id}>Delete</DeletePost>
-              <Link to={`/editPost/${post.id}`}>Edit</Link>
-            </div>
+        {posts.map((post) => {
+          console.log(post)
+          return (
+            <>
+              <div className="container post_list_container">
+              <Link to={`/post/${post.id}`}  key={post.id} value={post.id}>
+                <h2>{post.title}</h2>
+              </Link>
+                <div>
+                  <DeletePost  onClick={DeletePost} key={post.id} id={post.id}>Delete</DeletePost>
+                  <Link className="edit" to={`/editPost/${post.id}`}>Edit</Link>
+
+                </div>
+              </div>
+            </>
           )
         })}
       </>
